8e816b7eab0e0ef24b64556c862e62f995eacfd7
SantiagoPeres <santiagogperes@gmail.com>
2021-10-01 01:09:41 -0300

    App.js

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Prova PDM - Santiago Loureço Galhardo Peres</Text>
+      <Counter />
+      <ClearValue/>
+      
     </View>
   );
 }
 
+const useCounter = () => {
+  const [valorBotao, setValue] = useState(1022)
+
+  const setCount = () => {
+    setValue(valorBotao * 2);
+  }
+
+  return [valorBotao, setCount];
+}
+
+const useClearCounter = () =>{
+  const [clearValorBotao, setClearValue] = useState()
+  const setClearCount = () => {
+    setClearValue(1022)
+  }
+   
+  return [clearValorBotao, setClearCount];
+  
+}
+
+const ClearValue = () => {
+  const [clearValorBotao,setClearCounter] = useClearCounter();
+
+
+  return(
+    <View>
+      <Text style={styles.text}>{clearValorBotao}</Text>
+      <Button 
+        title="Clique aqui para reiniciar a contagem!"
+        onPress={() => setClearCounter()}></Button>
+    </View>
+  )
+}
+
+const Counter = () => {
+  const [valorBotao,setCounter] = useCounter();
+
+
+  return(
+    <View>
+      <Text style={styles.text}>{valorBotao}</Text>
+      <Button 
+        title="Clique aqui!"
+        onPress={() => setCounter()}></Button>
+    </View>
+  )
+}
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#ADD8E6",
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize:50,
+    color:'#fff',
+    textAlign:'center'
+  },
+  
 });
+
+
+
+export default App;
